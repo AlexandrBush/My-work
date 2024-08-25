837cd8d0a9bb45aee44bf714cf02150a1e27cbff
AlexandrBush <Alexandrbush@icloud.com>
2024-08-25 09:27:31 +0700

    Add files via upload

---
+++ Module 4.1.py
@@ -0,0 +1,7 @@
+from Fake_math import divide as fk_div
+from True_math import divide as tr_div
+
+print('result1 = ', fk_div(18, 3))
+print('result2 = ', fk_div(17, 0))
+print('result3 = ', tr_div(21, 7))
+print('result4 = ', tr_div(11, 0))
