4cd781e36e0f76d6c764b08b2d8c113407424876
AlexandrBush <Alexandrbush@icloud.com>
2024-09-06 16:19:51 +0700

    Add files via upload

---
+++ Module 7.4 file in op sist.py
@@ -0,0 +1,32 @@
+import os
+import time
+
+
+def list_files_in_directory(directory):
+    # Обход каталога с помощью os.walk
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            # Формирование полного пути к файлу
+            filepath = os.path.join(root, file)
+
+            # Получение времени последнего изменения файла
+            filetime = os.path.getmtime(filepath)
+            formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+
+            # Получение размера файла
+            filesize = os.path.getsize(filepath)
+
+            # Получение родительской директории файла
+            parent_dir = os.path.dirname(filepath)
+
+            # Вывод информации о файле
+            print(
+                f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time},'
+                f' Родительская директория: {parent_dir}')
+
+
+# Укажите путь к каталогу, который нужно обойти
+directory = "."  # Текущая директория проекта
+
+# Вызов функции для обхода каталога
+list_files_in_directory(directory)
