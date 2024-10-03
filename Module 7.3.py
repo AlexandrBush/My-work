a4023feb0a2901944c8c77d72be3c8d65c157009
AlexandrBush <Alexandrbush@icloud.com>
2024-10-03 15:53:32 +0700

    Add files via upload

--- Module 7.3.py
+++ Module 7.3.py
@@ -1,41 +1,68 @@
+import re
 
+class WordsFinder:
+    def __init__(self, *file_names):
+        """
+        Инициализация класса с переданными именами файлов.
+        :param file_names: Переменное количество аргументов с именами файлов.
+        """
+        self.file_names = file_names
 
-# Пример входных данных
-team1_num = 5
-team2_num = 6
-score_1 = 40
-score_2 = 42
-team1_time = 1552.512
-team2_time = 2153.31451
-tasks_total = 82
-time_avg = 45.2
+    def get_all_words(self):
+        """
+        Метод для получения всех слов из файлов.
+        :return: Словарь, где ключ - имя файла, значение - список слов в этом файле.
+        """
+        all_words = {}
+        punctuation = r"[',.=!?;:\s\-]"  # Регулярное выражение для удаления пунктуации
 
+        for file_name in self.file_names:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                text = file.read().lower()  # Считываем текст и переводим в нижний регистр
+                words = re.split(punctuation, text)  # Разбиваем текст на слова, удаляя пунктуацию
+                words = [word for word in words if word]  # Удаляем пустые строки
+                all_words[file_name] = words  # Сохраняем слова в словаре
 
-# Определение результата соревнования
-if score_1 > score_2 or (score_1 == score_2 and team1_time < team2_time):
-    challenge_result = 'Победа команды Мастера кода!'
-elif score_1 < score_2 or (score_1 == score_2 and team1_time > team2_time):
-    challenge_result = 'Победа команды Волшебники данных!'
-else:
-    challenge_result = 'Ничья!'
+        return all_words
 
+    def find(self, word):
+        """
+        Метод для поиска первого вхождения слова в каждом файле.
+        :param word: Искомое слово.
+        :return: Словарь, где ключ - имя файла, значение - позиция первого вхождения слова.
+        """
+        word = word.lower()  # Переводим искомое слово в нижний регистр
+        result = {}
 
-# Использование %:  Мы используем %d для вставки целых чисел.
+        for file_name, words in self.get_all_words().items():
+            if word in words:
+                result[file_name] = words.index(word)  # Находим индекс первого вхождения слова
 
-print("В команде Мастера кода участников: %d !" % team1_num)
-print("Итого сегодня в командах участников: %d и %d !" % (team1_num, team2_num))
+        return result
 
+    def count(self, word):
+        """
+        Метод для подсчета количества вхождений слова в каждом файле.
+        :param word: Искомое слово.
+        :return: Словарь, где ключ - имя файла, значение - количество вхождений слова.
+        """
+        word = word.lower()  # Переводим искомое слово в нижний регистр
+        result = {}
 
-# Использование format(): Мы используем {} для вставки значений.
-# Для форматирования чисел с плавающей точкой до одного знака после запятой используем :.1f.
+        for file_name, words in self.get_all_words().items():
+            result[file_name] = words.count(word)  # Считаем количество вхождений слова
 
-print("Команда Волшебники данных решила задач: {} !".format(score_2))
-print("Волшебники данных решили задачи за {:.1f} с !".format(team2_time))
-# print("Волшебники данных решили задачи за {} с !".format(team2_time))
+        return result
 
+# Пример использования класса
+if __name__ == "__main__":
+    finder = WordsFinder('test_file.txt')
 
-# Использование f-строк: Мы используем f"" для вставки значений переменных прямо в строку.
+    # Получаем все слова из файла
+    print(finder.get_all_words())
 
-print(f"Команды решили {score_1} и {score_2} задач.")
-print(f"Результат битвы: {challenge_result}!")
-print(f"Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!.")
+    # Находим позицию первого вхождения слова 'text'
+    print(finder.find('TEXT'))
+
+    # Считаем количество вхождений слова 'text'
+    print(finder.count('teXT'))
