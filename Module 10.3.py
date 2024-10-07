744178a6a7e207437cf405df64f80d16824227c1
AlexandrBush <Alexandrbush@icloud.com>
2024-10-07 12:53:16 +0700

    Update Module 10.3.py

--- Module 10.3.py
+++ Module 10.3.py
@@ -4,7 +4,7 @@ import time
 
 class Bank:
     def __init__(self):
-        self.balance = 0
+        self.balance = 0  # Начальный баланс
         self.lock = threading.Lock()
 
     def deposit(self):
@@ -13,6 +13,8 @@ class Bank:
             with self.lock:
                 self.balance += amount
                 print(f"Пополнение: {amount}. Баланс: {self.balance}")
+                if self.balance >= 500 and self.lock.locked():
+                    self.lock.release()
             time.sleep(0.001)
 
     def take(self):
@@ -25,20 +27,22 @@ class Bank:
                     print(f"Снятие: {amount}. Баланс: {self.balance}")
                 else:
                     print("Запрос отклонён, недостаточно средств")
-            time.sleep(0.001)
+                    self.lock.acquire()
 
+# Создаем объект банка
 bk = Bank()
 
 # Создаем потоки для методов deposit и take
-th1 = threading.Thread(target=bk.deposit)
-th2 = threading.Thread(target=bk.take)
+th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
 
 # Запускаем потоки
 th1.start()
 th2.start()
 
-# Ждем завершения потоков
+# Дожидаемся завершения потоков
 th1.join()
 th2.join()
 
+# Выводим итоговый баланс
 print(f'Итоговый баланс: {bk.balance}')
