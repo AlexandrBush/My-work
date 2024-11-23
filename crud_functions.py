20d79ead4bc77964e2b891eaef0e7bef3f910976
AlexandrBush <Alexandrbush@icloud.com>
2024-11-23 20:31:59 +0700

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -4,6 +4,7 @@ def initiate_db():
     conn = sqlite3.connect('products.db')
     cursor = conn.cursor()
 
+    # Создание таблицы Products, если она не существует
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Products (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -14,7 +15,7 @@ def initiate_db():
     )
     ''')
 
-    # Проверка на наличие записей в таблице
+    # Проверка на наличие записей в таблице Products
     cursor.execute('SELECT COUNT(*) FROM Products')
     count = cursor.fetchone()[0]
 
@@ -28,6 +29,17 @@ def initiate_db():
         ('Активированный уголь', 'Помощь при отравлении', 400, 'https://via.placeholder.com/150')
         ''')
 
+    # Создание таблицы Users, если она не существует
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        age INTEGER NOT NULL,
+        balance INTEGER NOT NULL
+    )
+    ''')
+
     conn.commit()
     conn.close()
 
@@ -40,3 +52,26 @@ def get_all_products():
 
     conn.close()
     return [{'id': product[0], 'title': product[1], 'description': product[2], 'price': product[3], 'image': product[4]} for product in products]
+
+def add_user(username, email, age):
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+
+    # Добавление нового пользователя с балансом 1000
+    cursor.execute('''
+    INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, 1000)
+    ''', (username, email, age))
+
+    conn.commit()
+    conn.close()
+
+def is_included(username):
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+
+    # Проверка наличия пользователя в таблице Users
+    cursor.execute('SELECT EXISTS(SELECT 1 FROM Users WHERE username = ?)', (username,))
+    result = cursor.fetchone()[0]
+
+    conn.close()
+    return bool(result)
