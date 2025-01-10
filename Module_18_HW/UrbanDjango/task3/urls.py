a30b3f9cfe15491be06260fe0912c274d9529e54
AlexandrBush <Alexandrbush@icloud.com>
2025-01-10 23:21:35 +0700

    Add files via upload

---
+++ Module_18_HW/UrbanDjango/task3/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.platform, name='platform'),
+    path('games/', views.games, name='games'),
+    path('cart/', views.cart, name='cart'),
+]
