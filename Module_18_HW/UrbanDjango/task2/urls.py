042e2924cdf9143b5713b7a9419040f8abbdd487
AlexandrBush <Alexandrbush@icloud.com>
2025-01-10 23:20:23 +0700

    Delete Module_18_HW/UrbanDjango directory

--- Module_18_HW/UrbanDjango/task2/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ClassBasedView, function_based_view
-
-urlpatterns = [
-    path('class-view/', ClassBasedView.as_view(), name='class_view'),
-    path('func-view/', function_based_view, name='func_view'),
-]
--- Module_18_HW/UrbanDjango/task3/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.platform, name='platform'),
-    path('games/', views.games, name='games'),
-    path('cart/', views.cart, name='cart'),
-]
